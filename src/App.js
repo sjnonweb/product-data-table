dc4cd42d4fd0907f7024428f3d7e1df73edf8b2b
sajan <sjnmishra8@gmail.com>
2017-07-04 01:34:44 +0530

    added controlled input form and data binding

--- src/App.js
+++ src/App.js
@@ -3,12 +3,30 @@ import React, { Component } from 'react';
 //import './App.css';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.handleFilterChange = this.handleFilterChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
+  }
+  
+  handleFilterChange(e) {
+    this.props.onFilterTextChange(e.target.value);
+  }
+
+  handleInStockChange(e) {
+    this.props.onInStockOnlyChange(e.target.checked);
+  }
+
   render() {
     return (
       <form>
-        <input type="text" placeholder="Search..." value={this.props.filterText} />
+        <input type="text" placeholder="Search..." value={this.props.filterText}
+          onChange={this.handleFilterChange}
+        />
         <p>
-          <input type="checkbox" checked={this.props.inStockOnly} />
+          <input type="checkbox" checked={this.props.inStockOnly}
+            onChange={this.handleInStockChange}
+          />
           {' '}
           Only show products in stock
         </p>
@@ -48,11 +66,10 @@ class ProductTable extends Component {
   render() {
     var row = [];
     var lastCategory = null;
-    this.props.products.forEach(function(product) {
-      if(product.name.indexOf(this.props.filterText) === -1 ||
-        (!product.stoked && this.props.inStockOnly)) {
-          return;
-        }
+    this.props.products.forEach((product) => {
+      if(product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)) {
+        return;
+      }
       if(product.category !== lastCategory) {
         row.push(<ProductCategoryRow category={product.category} key={product.category} />);
       }
@@ -82,8 +99,22 @@ class App extends Component {
       filterText: '',
       inStockOnly: false
     }
+    this.onFilterTextChange = this.onFilterTextChange.bind(this);
+    this.onInStockOnlyChange = this.onInStockOnlyChange.bind(this);
   }
   
+  onFilterTextChange(filterText) {
+    this.setState({
+      filterText
+    })
+  }
+
+  onInStockOnlyChange(inStockOnly) {
+    this.setState({
+      inStockOnly
+    })
+  }
+
   render() {
     var PRODUCTS = [
       { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
@@ -95,7 +126,12 @@ class App extends Component {
     ];
     return (
       <div className="App">
-        <SearchBar filterText={this.state.filterText} inStockOnly={this.state.inStockOnly} />
+        <SearchBar
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+          onFilterTextChange={this.onFilterTextChange}
+          onInStockOnlyChange={this.onInStockOnlyChange}
+        />
         <ProductTable
           products={PRODUCTS}
           filterText={this.state.filterText}
